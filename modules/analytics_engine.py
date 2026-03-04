f24e5b39515aa106a2a6767a7ccf0373fe61bc9a
Mohammed Kaif <153200967+Mohammedkaif2003@users.noreply.github.com>
2026-03-04 10:46:01 +0530

    Update analytics_engine.py

--- modules/analytics_engine.py
+++ modules/analytics_engine.py
@@ -121,8 +121,7 @@ def forecast_revenue(steps=6):
     else:
         trend = "Declining"
 
-    return history, forecast_values, conf_int, risk_level, round(volatility, 2), trend = forecast_revenue()
-    fig = plot_forecast(history, forecast, conf_int)
+    return monthly_data, forecast_values, conf_int, risk_level, round(volatility, 2), trend
 # -----------------------------
 # GENERATE SUMMARY (FOR PDF)
 # -----------------------------
